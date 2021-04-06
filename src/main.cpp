04ca0ad165b8336debc085bf96ca30d26ed84490
Artem Shoobovych <shybovycha@gmail.com>
2021-04-07 08:34:37 +1000

    Get rid of "using namespace" and mixing in stdio

--- src/main.cpp
+++ src/main.cpp
@@ -102,21 +102,15 @@ So, just calculate the right index for i (<latex>W_{j,i}</latex>, in the first f
 
 #include <irrlicht.h>
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
-
-using std::vector;
-
-using namespace irr;
-
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
 
 class HalfEdge {
 public:
-    HalfEdge(u32 e1, u32 e2, u32 v1, u32 v2, f32 w = 0.f) {
+    HalfEdge(irr::u32 e1, irr::u32 e2, irr::u32 v1, irr::u32 v2, irr::f32 w = 0.f) {
         this->e1 = e1;
         this->e2 = e2;
         this->v1 = v1;
@@ -124,7 +118,7 @@ public:
         this->w = w;
     }
 
-    void setPositions(vector3df e1, vector3df e2, vector3df v1, vector3df v2) {
+    void setPositions(irr::core::vector3df e1, irr::core::vector3df e2, irr::core::vector3df v1, irr::core::vector3df v2) {
         this->pe1 = e1;
         this->pe2 = e2;
         this->pv1 = v1;
@@ -132,16 +126,16 @@ public:
     }
 
 public:
-    u32 e1, e2; // edge vertices
-    u32 v1, v2; // side vertices
-    f32 w; // weight
-    vector3df pe1, pe2, pv1, pv2;
+    irr::u32 e1, e2; // edge vertices
+    irr::u32 v1, v2; // side vertices
+    irr::f32 w; // weight
+    irr::core::vector3df pe1, pe2, pv1, pv2;
 };
 
 class Triangle {
 public:
-    Triangle(u32 a, u32 b, u32 c, u16 meshBuffer = 0, u16 feature = -1) {
-        vertices = std::vector<u32>();
+    Triangle(irr::u32 a, irr::u32 b, irr::u32 c, irr::u16 meshBuffer = 0, irr::u16 feature = -1) {
+        vertices = std::vector<irr::u32>();
         vertices.push_back(a);
         vertices.push_back(b);
         vertices.push_back(c);
@@ -149,14 +143,14 @@ public:
         meshBufferId = meshBuffer;
     }
 
-    void setPositions(vector3df a, vector3df b, vector3df c) {
+    void setPositions(irr::core::vector3df a, irr::core::vector3df b, irr::core::vector3df c) {
         positions.push_back(a);
         positions.push_back(b);
         positions.push_back(c);
     }
 
-    std::vector<u32> commonVertices(Triangle& tri) {
-        std::vector<u32> res;
+    std::vector<irr::u32> commonVertices(Triangle& tri) {
+        std::vector<irr::u32> res;
 
         for (int i = 0; i < 3; i++) {
             for (int t = 0; t < 3; t++) {
@@ -169,29 +163,29 @@ public:
         return res;
     }
 
-    void assignFeatureId(s16 f) {
+    void assignFeatureId(irr::s16 f) {
         if (featureId < 0) {
             featureId = f;
         }
     }
 
 public:
-    std::vector<u32> vertices;
-    std::vector<vector3df> positions;
-    std::vector<vector2di> UVs;
+    std::vector<irr::u32> vertices;
+    std::vector<irr::core::vector3df> positions;
+    std::vector<irr::core::vector2di> UVs;
 
-    s16 featureId;
-    u16 meshBufferId;
+    irr::s16 featureId;
+    irr::u16 meshBufferId;
 };
 
-vector2di findCoVerticesForEdge(u16 e1, u16 e2, u16* indices, u16 indicesCnt, S3DVertex* vertices, f32 threshold = 0.001f) {
-    s16 V[] = { -1, -1 };
-    s16 cnt = 0;
+irr::core::vector2di findCoVerticesForEdge(irr::u16 e1, irr::u16 e2, irr::u16* indices, irr::u16 indicesCnt, irr::video::S3DVertex* vertices, irr::f32 threshold = 0.001f) {
+    irr::s16 V[] = { -1, -1 };
+    irr::s16 cnt = 0;
 
-    for (u16 v = 0; v < indicesCnt; v += 3) {
-        u32 a = indices[v];
-        u32 b = indices[v + 1];
-        u32 c = indices[v + 2];
+    for (irr::u16 v = 0; v < indicesCnt; v += 3) {
+        irr::u32 a = indices[v];
+        irr::u32 b = indices[v + 1];
+        irr::u32 c = indices[v + 2];
 
         /*if (a == e1 && c == e2)
             V[cnt++] = b; else
@@ -242,11 +236,11 @@ vector2di findCoVerticesForEdge(u16 e1, u16 e2, u16* indices, u16 indicesCnt, S3
         }
     }
 
-    return vector2di(V[0], V[1]);
+    return irr::core::vector2di(V[0], V[1]);
 }
 
-HalfEdge* createHaldEdge(u32 e1, u32 e2, u16* indices, u32 indexCount, S3DVertex* vertices) {
-    vector2di coVerts = findCoVerticesForEdge(e1, e2, indices, indexCount, vertices);
+HalfEdge* createHaldEdge(irr::u32 e1, irr::u32 e2, irr::u16* indices, irr::u32 indexCount, irr::video::S3DVertex* vertices) {
+    irr::core::vector2di coVerts = findCoVerticesForEdge(e1, e2, indices, indexCount, vertices);
 
     if (coVerts.Y < 0) {
         coVerts.Y = coVerts.X;
@@ -254,18 +248,18 @@ HalfEdge* createHaldEdge(u32 e1, u32 e2, u16* indices, u32 indexCount, S3DVertex
         return NULL;
     }
 
-    u32 v1 = coVerts.X;
-    u32 v2 = coVerts.Y;
+    irr::u32 v1 = coVerts.X;
+    irr::u32 v2 = coVerts.Y;
 
-    vector3df a1 = (vertices[v1].Pos - vertices[e1].Pos);
-    vector3df a2 = (vertices[v2].Pos - vertices[e1].Pos);
+    irr::core::vector3df a1 = (vertices[v1].Pos - vertices[e1].Pos);
+    irr::core::vector3df a2 = (vertices[v2].Pos - vertices[e1].Pos);
 
-    vector3df edge = (vertices[e2].Pos - vertices[e1].Pos);
+    irr::core::vector3df edge = (vertices[e2].Pos - vertices[e1].Pos);
 
-    vector3df n1 = a1.crossProduct(edge);
-    vector3df n2 = a2.crossProduct(edge);
+    irr::core::vector3df n1 = a1.crossProduct(edge);
+    irr::core::vector3df n2 = a2.crossProduct(edge);
 
-    f32 w = n1.dotProduct(n2) / (n1.getLength() * n2.getLength());
+    irr::f32 w = n1.dotProduct(n2) / (n1.getLength() * n2.getLength());
 
     HalfEdge* he = new HalfEdge(indices[e1], indices[e2], v1, v2, w);
 
@@ -274,23 +268,23 @@ HalfEdge* createHaldEdge(u32 e1, u32 e2, u16* indices, u32 indexCount, S3DVertex
     return he;
 }
 
-std::vector<HalfEdge*> fillHalfEdges(IMesh* mesh) {
+std::vector<HalfEdge*> fillHalfEdges(irr::scene::IMesh* mesh) {
     std::vector<HalfEdge*> halfEdges;
 
-    u16 meshBufferCount = mesh->getMeshBufferCount();
+    irr::u16 meshBufferCount = mesh->getMeshBufferCount();
 
-    for (u16 i = 0; i < meshBufferCount; ++i) {
-        IMeshBuffer* mb = mesh->getMeshBuffer(i);
+    for (irr::u16 i = 0; i < meshBufferCount; ++i) {
+        irr::scene::IMeshBuffer* mb = mesh->getMeshBuffer(i);
 
-        u16* indices = mb->getIndices();
-        u32 indexCount = mb->getIndexCount();
+        irr::u16* indices = mb->getIndices();
+        irr::u32 indexCount = mb->getIndexCount();
 
-        S3DVertex* vertices = (S3DVertex*) mb->getVertices();
+        irr::video::S3DVertex* vertices = (irr::video::S3DVertex*) mb->getVertices();
 
-        for (u32 e = 0; e < indexCount; e += 3) {
+        for (irr::u32 e = 0; e < indexCount; e += 3) {
             {
-                u32 e1 = indices[e];
-                u32 e2 = indices[e + 1];
+                irr::u32 e1 = indices[e];
+                irr::u32 e2 = indices[e + 1];
 
                 HalfEdge* he = createHaldEdge(e1, e2, indices, indexCount, vertices);
 
@@ -302,8 +296,8 @@ std::vector<HalfEdge*> fillHalfEdges(IMesh* mesh) {
             }
 
             {
-                u32 e1 = indices[e + 1];
-                u32 e2 = indices[e + 2];
+                irr::u32 e1 = indices[e + 1];
+                irr::u32 e2 = indices[e + 2];
 
                 HalfEdge* he = createHaldEdge(e1, e2, indices, indexCount, vertices);
 
@@ -315,8 +309,8 @@ std::vector<HalfEdge*> fillHalfEdges(IMesh* mesh) {
             }
 
             {
-                u32 e1 = indices[e + 2];
-                u32 e2 = indices[e];
+                irr::u32 e1 = indices[e + 2];
+                irr::u32 e2 = indices[e];
 
                 HalfEdge* he = createHaldEdge(e1, e2, indices, indexCount, vertices);
 
@@ -332,12 +326,12 @@ std::vector<HalfEdge*> fillHalfEdges(IMesh* mesh) {
     return halfEdges;
 }
 
-std::vector<HalfEdge*> detectSeams(std::vector<HalfEdge*> halfEdges, f32 Bu = 0.995f, f32 Bl = 0.92f) {
+std::vector<HalfEdge*> detectSeams(std::vector<HalfEdge*> halfEdges, irr::f32 Bu = 0.995f, irr::f32 Bl = 0.92f) {
     std::vector<HalfEdge*> paths;
     HalfEdge* selected = 0;
-    std::vector< std::vector<S3DVertex*> > features;
+    std::vector< std::vector<irr::video::S3DVertex*> > features;
 
-    for (u32 i = 0; i < halfEdges.size(); ++i) {
+    for (irr::u32 i = 0; i < halfEdges.size(); ++i) {
         HalfEdge* he = halfEdges[i];
 
         if (he->w >= Bu) {
@@ -356,31 +350,31 @@ std::vector<HalfEdge*> detectSeams(std::vector<HalfEdge*> halfEdges, f32 Bu = 0.
     return paths;
 }
 
-std::vector< std::vector<Triangle> > growFeatures(IMesh* mesh) {
+std::vector< std::vector<Triangle> > growFeatures(irr::scene::IMesh* mesh) {
     std::vector<HalfEdge*> halfEdges = fillHalfEdges(mesh);
     std::vector<HalfEdge*> paths = detectSeams(halfEdges);
 
-    std::vector< std::vector<u16> > connectedTrianglesList;
+    std::vector< std::vector<irr::u16> > connectedTrianglesList;
 
     std::vector<Triangle> triangles;
     std::vector< std::vector<Triangle> > features;
 
-    u16 meshBufferCount = mesh->getMeshBufferCount();
+    irr::u16 meshBufferCount = mesh->getMeshBufferCount();
 
-    for (u16 i = 0; i < meshBufferCount; ++i) {
-        IMeshBuffer* mb = mesh->getMeshBuffer(i);
+    for (irr::u16 i = 0; i < meshBufferCount; ++i) {
+        irr::scene::IMeshBuffer* mb = mesh->getMeshBuffer(i);
 
-        u16* indices = mb->getIndices();
-        u32 indexCount = mb->getIndexCount();
+        irr::u16* indices = mb->getIndices();
+        irr::u32 indexCount = mb->getIndexCount();
 
-        S3DVertex* vertices = (S3DVertex*) mb->getVertices();
+        irr::video::S3DVertex* vertices = (irr::video::S3DVertex*) mb->getVertices();
 
-        for (u32 t = 0; t < indexCount; t += 3) {
+        for (irr::u32 t = 0; t < indexCount; t += 3) {
             // building connected triangles list with just one enhancement
             // if two triangles do have common edge and that edge belongs to seams
             // then each triangle is assigned new feature ID.
             // NOTE: if triangle has feature ID assigned already - it does not get new one. ever.
-            S3DVertex a = vertices[indices[t]], b = vertices[indices[t + 1]], c = vertices[indices[t + 2]];
+            irr::video::S3DVertex a = vertices[indices[t]], b = vertices[indices[t + 1]], c = vertices[indices[t + 2]];
 
             Triangle triangle(indices[t], indices[t + 1], indices[t + 2], i);
 
@@ -388,24 +382,24 @@ std::vector< std::vector<Triangle> > growFeatures(IMesh* mesh) {
 
             // u16 triangleIndex = triangles.size();
 
-            connectedTrianglesList.push_back(std::vector<u16>());
+            connectedTrianglesList.push_back(std::vector<irr::u16>());
 
             triangles.push_back(triangle);
         }
     }
 
-    u16 T = triangles.size();
-    u16 P = paths.size();
+    irr::u16 T = triangles.size();
+    irr::u16 P = paths.size();
 
-    printf("Generated %d triangles\n", T);
+    std::cout << "Generated " << T << " triangles\n";
 
-    for (u16 t1 = 0; t1 < T; ++t1) {
-        for (u16 t2 = 0; t2 < T; ++t2) {
+    for (irr::u16 t1 = 0; t1 < T; ++t1) {
+        for (irr::u16 t2 = 0; t2 < T; ++t2) {
             if (t1 == t2) {
                 continue;
             }
 
-            std::vector<u32> c = triangles[t1].commonVertices(triangles[t2]);
+            std::vector<irr::u32> c = triangles[t1].commonVertices(triangles[t2]);
 
             if (c.size() > 1) {
                 connectedTrianglesList[t1].push_back(t2);
@@ -415,15 +409,15 @@ std::vector< std::vector<Triangle> > growFeatures(IMesh* mesh) {
 
     // printf("Connected list size: %d\n", connectedTrianglesList.size());
 
-    for (u16 t1 = 0; t1 < connectedTrianglesList.size(); ++t1) {
-        for (u16 t2 = 0; t2 < connectedTrianglesList[t1].size(); ++t2) {
-            u16 ti1 = t1, ti2 = connectedTrianglesList[t1][t2];
+    for (irr::u16 t1 = 0; t1 < connectedTrianglesList.size(); ++t1) {
+        for (irr::u16 t2 = 0; t2 < connectedTrianglesList[t1].size(); ++t2) {
+            irr::u16 ti1 = t1, ti2 = connectedTrianglesList[t1][t2];
 
-            std::vector<u32> c = triangles[ti1].commonVertices(triangles[ti2]);
+            std::vector<irr::u32> c = triangles[ti1].commonVertices(triangles[ti2]);
 
             int featureFound = 0;
 
-            for (u16 j = 0; j < P; ++j) {
+            for (irr::u16 j = 0; j < P; ++j) {
                 if (paths[j]->e1 == c[0] || paths[j]->e2 == c[0] || paths[j]->e1 == c[1] || paths[j]->e2 == c[2]) {
                     featureFound = 1;
 
@@ -470,19 +464,22 @@ void renderFeaturesToFiles(std::vector< std::vector<Triangle> > features) {
     char* filename = new char[255];
 
     for (int i = 0; i < features.size(); ++i) {
-        sprintf(filename, "tmp_%d.obj", i);
+        std::string filename;
+        std::ostringstream os;
 
-        FILE* f = fopen(filename, "w");
+        os << "tmp_" << i << ".obj";
 
-        std::vector<u16> indices;
-        u32 indexCount = 0;
+        std::ofstream f(os.str());
+
+        std::vector<irr::u16> indices;
+        irr::u32 indexCount = 0;
 
         for (int t = 0; t < features[i].size(); ++t) {
             Triangle tri = features[i][t];
 
-            fprintf(f, "v %f %f %f\n", tri.positions[0].X, tri.positions[0].Y, tri.positions[0].Z);
-            fprintf(f, "v %f %f %f\n", tri.positions[1].X, tri.positions[1].Y, tri.positions[1].Z);
-            fprintf(f, "v %f %f %f\n", tri.positions[2].X, tri.positions[2].Y, tri.positions[2].Z);
+            f << "v " << tri.positions[0].X << tri.positions[0].Y << tri.positions[0].Z << "\n";
+            f << "v " << tri.positions[1].X << tri.positions[1].Y << tri.positions[1].Z << "\n";
+            f << "v " << tri.positions[2].X << tri.positions[2].Y << tri.positions[2].Z << "\n";
 
             indices.push_back(indexCount);
             indices.push_back(indexCount + 1);
@@ -492,42 +489,47 @@ void renderFeaturesToFiles(std::vector< std::vector<Triangle> > features) {
         }
 
         for (int t = 0; t < indices.size(); t += 3) {
-            fprintf(f, "f %d// %d// %d//\n", indices[t], indices[t + 1], indices[t + 2]);
+            f << "f " << indices[t] << "//" << indices[t + 1] << "//" << indices[t + 2] << "//\n";
         }
 
-        fclose(f);
+        f.close();
     }
 }
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        printf("Please, run %s <3D model file> <texture base filename>\n", argv[0]);
+        std::cout << "Please, run " << argv[0] << " <3D model file> <texture base filename>\n";
 
         return 0;
     }
 
-    IrrlichtDevice *device =
-            createDevice(video::EDT_BURNINGSVIDEO, dimension2d<u32>(1024, 768), 32,
+    irr::IrrlichtDevice *device =
+            irr::createDevice(irr::video::EDT_BURNINGSVIDEO, irr::core::dimension2d<irr::u32>(1024, 768), 32,
                     false, false, true, 0);
 
-    if (!device)
+    if (!device) {
+        std::cerr << "Could not initialize video device\n";
+
         return 1;
+    }
 
     device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
 
-    IVideoDriver* driver = device->getVideoDriver();
-    ISceneManager* smgr = device->getSceneManager();
-    IGUIEnvironment* guienv = device->getGUIEnvironment();
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    irr::scene::ISceneManager* smgr = device->getSceneManager();
+    irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-    IAnimatedMesh* modelMesh = smgr->getMesh(argv[1]);
+    irr::scene::IAnimatedMesh* modelMesh = smgr->getMesh(argv[1]);
 
     if (!modelMesh) {
+        std::cerr << "Could not read model file " << argv[1] << "\n";
+
         device->drop();
 
         return 1;
     }
 
-    IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(modelMesh);
+    irr::scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(modelMesh);
 
     //smgr->addLightSceneNode(0, vector3df(0, 100, 0), SColorf(100.f, 100.f, 100.f));
 
@@ -540,9 +542,9 @@ int main(int argc, char** argv) {
     //     //node->setScale(vector3df(0.f, 0.f, 0.f));
     // }
 
-    node->setScale(vector3df(10.f, 10.f, 10.f));
+    node->setScale(irr::core::vector3df(10.f, 10.f, 10.f));
 
-    IMesh* mesh = modelMesh->getMesh(0);
+    irr::scene::IMesh* mesh = modelMesh->getMesh(0);
     std::vector<HalfEdge*> halfEdges = fillHalfEdges(mesh);
 
     std::vector<HalfEdge*> seams = detectSeams(halfEdges);
@@ -551,13 +553,15 @@ int main(int argc, char** argv) {
     smgr->addCameraSceneNodeFPS(0, 50.f, 0.0125f);
     smgr->getActiveCamera()->setNearValue(0.01);
 
-    u32 edgeCount = 0;
+    irr::u32 edgeCount = 0;
 
-    for (u16 i = 0; i < modelMesh->getMeshBufferCount(); ++i) {
+    for (irr::u16 i = 0; i < modelMesh->getMeshBufferCount(); ++i) {
         edgeCount += modelMesh->getMeshBuffer(i)->getIndexCount();
     }
 
-    printf("Seams: %ld\nEdge count: %u\nFeature count: %lu\n", seams.size(), edgeCount, features.size());
+    std::cout << "Seams: " << seams.size() << "\n";
+    std::cout << "Edge count: " << edgeCount << "\n";
+    std::cout << "Feature count: " << features.size() << "\n";
 
     renderFeaturesToFiles(features);
 
